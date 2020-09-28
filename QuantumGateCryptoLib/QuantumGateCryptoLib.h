31f5bc794c9b3da79fe1ebbaa0600b85a4d27cc0
Karel Donk <karel@miraesoft.com>
2020-09-28 18:22:35 -0300

    Added halfsiphash

--- QuantumGateCryptoLib/QuantumGateCryptoLib.h
+++ QuantumGateCryptoLib/QuantumGateCryptoLib.h
@@ -13,8 +13,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	int siphash(const uint8_t *in, const size_t inlen, const uint8_t *k,
-				uint8_t *out, const size_t outlen);
+	int halfsiphash(const uint8_t* in, const size_t inlen, const uint8_t* k,
+					uint8_t* out, const size_t outlen);
+	int siphash(const uint8_t* in, const size_t inlen, const uint8_t* k,
+				uint8_t* out, const size_t outlen);
 #ifdef __cplusplus
 }
 #endif
